27bc6c3c84d8bb5297cd4f17fd3ff725e88a66d6
Marcin Serwin <toxyxer@gmail.com>
2019-01-20 01:18:51 +0100

    Add empty kernel function with transferred data

--- src/render.cu
+++ src/render.cu
@@ -2,12 +2,46 @@
 #include "render.h"
 #include <vector>
 
+static constexpr int CHUNK_SIZE = 32;
+
+
+#define gpuErrchk(ans)                                                         \
+    { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+                line);
+        if (abort)
+            exit(code);
+    }
+}
+
+static __global__ void kernel(int width, int height, RGBColor *img, Sphere *spheres) { }
+
+
 Image render(std::vector<Sphere> const &spheres) {
     Image img(1024, 768);
-    for (int i = 0; i < img.width(); i++) {
-        for (int j = 0; j < img.height(); j++) {
-            img(i, j) = RGBColor(1, 0.5, 1);
-        }
-    }
+    
+
+    RGBColor *cudaPixels;
+    gpuErrchk(cudaMalloc(&cudaPixels, sizeof(RGBColor) * img.width() * img.height()));
+
+    Sphere *cudaSpheres;
+    gpuErrchk(cudaMalloc(&cudaSpheres, sizeof(Sphere) * spheres.size()));
+    gpuErrchk(cudaMemcpy(cudaSpheres, spheres.data(), sizeof(Sphere) * spheres.size(),
+        cudaMemcpyHostToDevice));
+
+    uint32_t gridX = (img.width() + CHUNK_SIZE - 1) / CHUNK_SIZE;
+    uint32_t gridY = (img.height() + CHUNK_SIZE - 1) / CHUNK_SIZE;
+
+    kernel<<<{gridX, gridY}, {CHUNK_SIZE, CHUNK_SIZE}>>>(img.width(), img.height(), cudaPixels, cudaSpheres);
+
+    gpuErrchk(cudaMemcpy(img.raw(), cudaPixels,
+                         sizeof(RGBColor) * img.width() * img.height(),
+                         cudaMemcpyDeviceToHost));
+    cudaFree(cudaPixels);
+    cudaFree(cudaSpheres);
+
     return img;
 }
