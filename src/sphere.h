c9f7253edf8963afcdf773a7b1f52f8a6943d98b
wowol <wbuczek07@gmail.com>
2019-01-23 14:50:51 +0100

    Rework hits_ray function

--- src/sphere.h
+++ src/sphere.h
@@ -12,34 +12,37 @@
 #endif
 
 class Sphere {
-  public:
-    HD Sphere(Vector3 pos, float r, Material const &material)
-        : position(pos), radius(r), material(material) {}
-    HD float get_radius() const { return radius; }
-    HD Vector3 get_position() const { return position; }
-    HD Material get_material() const { return material; }
+ public:
+  HD Sphere(Vector3 pos, float r, Material const &material)
+      : position(pos), radius(r), material(material) {}
+  HD float get_radius() const { return radius; }
+  HD Vector3 get_position() const { return position; }
+  HD Material get_material() const { return material; }
 
-    HD bool hits_ray(const Ray &ray) {
-        Vector3 oc(ray.get_position(), position);
-        return oc.cross_product(ray.get_direction()).length() < radius;
-    }
+  HD bool hits_ray(const Ray &ray) {
+    Vector3 oc(ray.get_position(), position);
+    float a = ray.get_direction().scalar_product(ray.get_direction());
+    float b = 2.0f * oc.scalar_product(ray.get_direction());
+    float c = oc.scalar_product(oc) - radius*radius;
+    float delta = b*b - 4*a*c;
+    return delta >= 0;
+  }
 
-    HD Vector3 get_intersection_point(Ray const &ray) const {
-        Vector3 oc(ray.get_position(), position);
-        float center_distance = oc.cross_product(ray.get_direction()).length();
-        float position_distance = oc.scalar_product(ray.get_direction());
+  HD Vector3 get_intersection_point(Ray const &ray) const {
+    Vector3 oc(ray.get_position(), position);
+    float center_distance = oc.cross_product(ray.get_direction()).length();
+    float position_distance = oc.scalar_product(ray.get_direction());
 
-        float offset =
-            sqrtf(radius * radius - center_distance * center_distance);
+    float offset = sqrtf(radius * radius - center_distance * center_distance);
 
-        return ray.get_position() +
-               ray.get_direction() * (position_distance - offset);
-    }
+    return ray.get_position() +
+           ray.get_direction() * (position_distance - offset);
+  }
 
-  private:
-    Vector3 position;
-    float radius;
-    Material material;
+ private:
+  Vector3 position;
+  float radius;
+  Material material;
 };
 
 #endif
