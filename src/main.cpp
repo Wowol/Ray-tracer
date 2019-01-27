42f83db11004d3df680cf43e5c5c82c9ecc9d7c2
wowol <wbuczek07@gmail.com>
2019-01-27 19:58:49 +0100

    Add random tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include <vector>
 #include "camera.h"
 #include "color.h"
@@ -64,6 +65,60 @@ void vector_test() {
     // std::cout << sphere.hits_ray(ray) << std::endl;
 }
 
+void random_test() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    Vector3 camera_position(0.0f, 4.0f, 0.0f);
+    Vector3 vector_to_screen(0, 0, 1);
+
+    float width = 1.6f;
+    float height = 0.9f;
+
+    Camera camera(camera_position, vector_to_screen, width, height);
+
+    int number_of_spheres = 1000;
+    int min_x = -40;
+    int max_x = 40;
+    int min_y = 0;
+    int max_y = 4;
+    int min_z = 10;
+    int max_z = 200;
+    int min_radius = 0.3f;
+    int max_radius = 3.0f;
+
+    std::vector<Light> lights = {Light(Vector3(-10, 10, 0.0f), 0.8),
+                                 Light(Vector3(0, 100, 300), 0.8)};
+
+    std::uniform_real_distribution<float> random_x(min_x, max_x);
+    std::uniform_real_distribution<float> random_y(min_y, max_y);
+    std::uniform_real_distribution<float> random_z(min_z, max_z);
+    std::uniform_real_distribution<float> random_radius(min_radius, max_radius);
+
+    std::uniform_real_distribution<float> random_0_1(0, 1);
+
+    std::uniform_real_distribution<float> random_0_0_0_5(0, 0.05f);
+
+    std::vector<Sphere> spheres;
+
+    for (int x = 0; x < number_of_spheres; x++) {
+        Sphere sphere(Vector3(random_x(gen), random_y(gen), random_z(gen)), random_radius(gen), Material(RGBColor(random_0_1(gen), random_0_1(gen), random_0_1(gen)), random_0_0_0_5(gen), 0));
+        bool intersect = false;
+        for (int y = 0; y < x; y++) {
+            if (spheres[y].get_position().distance(sphere.get_position()) < spheres[y].get_radius() + sphere.get_radius()) {
+                intersect = true;
+                break;
+            }
+        }
+        if (!intersect) {
+            spheres.push_back(sphere);
+        } else {
+            x--;
+        }
+    }
+
+    render(spheres, lights, camera).writePNG("out.png");
+}
+
 void render_test() {
     Vector3 position(0.0f, 4.0f, 0.0f);
     Vector3 vector_to_screen(0.0f, 0.0f, 1.0f);
@@ -94,6 +149,7 @@ void render_test() {
 int main() {
     // camera_test();
     // vector_test();
-    render_test();
+    // render_test();
+    random_test();
     return 0;
 }
